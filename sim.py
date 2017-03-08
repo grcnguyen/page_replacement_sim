f1072978c912b4b17e40d0ebf5aea03c82c11642
Grace Nguyen <grcknguyen@gmail.com>
2017-03-07 18:36:13 -0800

    Finished LFU and Second chance

--- sim.py
+++ sim.py
@@ -1,8 +1,7 @@
 import argparse
 import sys
 import collections
-
-#Page replacement algorithms
+from random import *
 
 ##########################################################################################
 
@@ -12,27 +11,27 @@ import collections
 
 def lru(input, num_pf):
     pf_count = 0
-    p_frames = collections.deque()  #list of page frames consisting of pairs (page number, last usage)
+    p_frames = collections.deque()
     
-    #print 'input:', input
     f = open("lru_page_faults.log", "w")
     original_stderr = sys.stderr
     sys.stderr = f
     for request in input:
-        #print 'Request:', request
         try:
             p_frames.remove(request)
             p_frames.appendleft(request)
         except ValueError:
             if len(p_frames) < num_pf:
                 p_frames.appendleft(request)
+                sys.stderr.write('%s' %request)
+                sys.stderr.write('\n')
+                pf_count = pf_count + 1
             else:
                 fault = p_frames.pop()
                 p_frames.appendleft(request)
                 sys.stderr.write('%s' %fault)
                 sys.stderr.write('\n')
                 pf_count = pf_count + 1
-                    #print 'P-frames:', p_frames
     sys.stderr = original_stderr
     f.close()
     print 'Number of Page Faults:', pf_count
@@ -40,7 +39,7 @@ def lru(input, num_pf):
 
 ##########################################################################################
 
-                                        #Second Chance
+                                    #Second Chance (Clock)
 
 ##########################################################################################
 
@@ -49,10 +48,42 @@ def sc(input, num_pf):
     original_stderr = sys.stderr
     sys.stderr = f
     
-    print 'Simulate Second Chance with', num_pf, 'page frames on', input
+    p_frames = []
+    ref_bits = []
+    pf_count = 0
+    clock_hand = 0
     
+    for request in input:
+    	try:
+    		i = p_frames.index(request) #searches for index of requested item
+    		ref_bits[i] = 1 #changes reference bit of requested item to 1
+    	except ValueError:
+            if len(p_frames) < num_pf: #filling empty clock
+                p_frames.append(request)
+                ref_bits.append(1)
+                sys.stderr.write('%s' %request)
+                sys.stderr.write('\n')
+                pf_count = pf_count + 1
+            else: #search and replace
+                while(1): #loop to search for page to replace    
+                    if ref_bits[clock_hand] == 0: 
+                        sys.stderr.write('%s' %p_frames[clock_hand])
+                        sys.stderr.write('\n')
+                        p_frames[clock_hand] = request 
+                        ref_bits[clock_hand] = 1 
+                        pf_count = pf_count + 1
+                        break
+                    else:
+                        ref_bits[clock_hand] = 0
+                    clock_hand = clock_hand + 1
+                    if clock_hand >= num_pf:
+                        clock_hand = 0
+            clock_hand = clock_hand + 1
+            if clock_hand >= num_pf:
+                clock_hand = 0
     sys.stderr = original_stderr
     f.close()
+    print 'Number of Page Faults:', pf_count
 
 
 ##########################################################################################
@@ -61,68 +92,94 @@ def sc(input, num_pf):
 
 ##########################################################################################
 
-def find_smallest(frames): #Helper function that returns INDEX of smallest count
-    max_count = 0
-    lru = -1
+def find_least(frames):
+    min = frames[0]
+    min_ind = 0
     for i in range(0, len(frames)):
-        if frames[i][1] > max_count:
-            max_count = frames[i][1]
-            lru = i
-    return lru
-
-def incr_count (page): #Helper function to increase the count of everything in current page frames
-    page[1] = page[1]+1
+        if frames[i] < min:
+            min = frames[i]
+            min_ind = i
+    return min_ind
 
 def lfu(input, num_pf):
     f = open("lfu_page_faults.log", "w")
     original_stderr = sys.stderr
     sys.stderr = f
-    p_reqs = []
-    p_frames = []  #list of page frames consisting of pairs (page number, last usage)
     
-    #convert input into pairs to keep track of last usage
-    for x in input:
-        p_reqs.append([x, 0])
-    
-    for request in p_reqs:
-        #print 'Request:', request
+    p_frames = []  #list of page frames
+    p_frames_count = [] #stored as pairs [# times used, age]
+    pf_count = 0
+
+    for request in input:
         try:
-            if p_reqs[0] == 
-            p_frames.remove(request)
-            p_frames.appendleft(request)
-        except ValueError:
-            if len(p_frames) < num_pf:
-                p_frames.appendleft(request)
-            else:
-                fault = p_frames.pop()
-                p_frames.appendleft(request)
-                sys.stderr.write('%s' %fault)
+            i = p_frames.index(request) #if index can be found, not a page fault
+            p_frames_count[i] = p_frames_count[i]+1
+            p_frames.pop(i)
+            p_frames.append(request)
+            move_count = p_frames_count.pop(i)
+            p_frames_count.append(move_count)
+        except ValueError: #page fault scenarios
+            if len(p_frames) < num_pf: #fill up page table
+                p_frames.append(request)
+                p_frames_count.append(0)
+                sys.stderr.write('%s' %request)
                 sys.stderr.write('\n')
                 pf_count = pf_count + 1
-    #print 'P-frames:', p_frames
+            else:
+                fault = find_least(p_frames_count) #returns index of the least used
+                sys.stderr.write('%s' %p_frames[fault])
+                sys.stderr.write('\n')
+                p_frames.pop(fault)
+                p_frames_count.pop(fault)
+                p_frames.append(request)
+                p_frames_count.append(0)
+                pf_count = pf_count + 1
     sys.stderr = original_stderr
     f.close()
+    print 'Number of Page Faults:', pf_count
 
 
 ##########################################################################################
 
-                                            #Random
+                                        #Random
 
 ##########################################################################################
 
 def random(input, num_pf):
-    print 'Simulate random page replacement with', num_pf, 'page frames on', input
-
+    f = open("random_faults.log", "w")
+    original_stderr = sys.stderr
+    sys.stderr = f
+    
+    p_frames = []
+    pf_count = 0
+    
+    for request in input:
+        try:
+            p_frames.index(request)
+        except ValueError:
+            if len(p_frames) < num_pf:
+                p_frames.append(request)
+                sys.stderr.write('%s' %request)
+                sys.stderr.write('\n')
+                pf_count = pf_count + 1
+            else:
+                i = randint(0, num_pf-1)
+                sys.stderr.write('%s' %p_frames[i])
+                sys.stderr.write('\n')
+                p_frames[i] = request 
+                pf_count = pf_count + 1             
+    sys.stderr = original_stderr
+    f.close()
+    print 'Number of Page Faults:', pf_count
 
 
 #========================================================================================#
-
-
 ##########################################################################################
 
                                 #Parsing Command Line Arguments
 
 ##########################################################################################
+#========================================================================================#
 
 #Parse command line arguments to select algorithm and page frame number
 parser = argparse.ArgumentParser()
@@ -162,13 +219,3 @@ elif args.alg == 'random':
     random(inp_list, num_page_frames)
 else:
     print 'Must specify a valid algorithm: lru, sc, lfu, random'
-
-
-
-
-#OUTPUT: The output of your program will be every page number that was not found to be in the cache. In other words, the output of your program will be a sequence of page numbers that represents all the incoming requests that resulted in a page fault.
-#you should be able to get two numbers from the unix command line (by counting the number of lines read from the input file, and the number of lines produced by your simulator).
-#The first of these numbers is the total number of page/block requests your simulator program has received (you get this by counting the number of valid lines in your input file)
-#he second number is how many of these page requests did result in a PAGE FAULTS (you get this by counting the number of lines produced as OUTPUT by your program - which is faithfully reproducing the page replacement algorithm's behavior).
-#print out faults to stderr
-#total error needs to be within 25
